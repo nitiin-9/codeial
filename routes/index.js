9b226dbb370573055b1d9ccf9fa0efc16ce1c802
nithin <nithink782@gmail.com>
2020-12-18 13:10:47 +0530

    added user controller to be acceseed users routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const homeController = require('../controllers/home_controllers');
  console.log('router is loaded');
 
- router.get('/',homeController.home);
+ router.get('/',homeController.home); 
+ router.use('/users',require('./users'));
+ // for any other further routes
+ // router.use('/routerName', require('./routerFile));
 module.exports= router;
