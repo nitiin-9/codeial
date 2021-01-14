d35e22263a6f5afcd4e9208e584891fe125bfa58
nithin <nithink782@gmail.com>
2021-01-14 19:02:24 +0530

    create posts controller

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const homeController = require('../controllers/home_controllers');
  console.log('router is loaded');
 
  router.get('/',homeController.home); 
- router.use('/users',require('./users'));
+ router.use('/users',require('./users')); 
+ router.use('/posts', require('./posts'));
  // for any other further routes
  // router.use('/routerName', require('./routerFile));
 module.exports= router;
---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router(); 
+
+
+const postsController = require('../controllers/posts_controllers');
+
+router.post('/create', postsController.create);
+
+  module.exports = router;
