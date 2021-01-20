6f31b5e0949c89c5e8eae452cb07c7b5e872c126
nithin <nithink782@gmail.com>
2021-01-20 20:01:11 +0530

    mailer set up to send mails

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,26 @@
+const nodeMailer = require('../config/nodemailer') ;
+
+// another way of exporting a method
+
+exports.newComment = (comment)  => {
+
+     let htmlString = nodeMailer.renderTemplate({comment:comment} , '/comments/new_comment.ejs');
+    nodeMailer.transporter.sendMail({
+          from :'nithinparipelli2324@gmail.com' ,
+          to : comment.user.email,
+          subject:'new comment published', 
+
+          html : htmlString
+
+
+    },(err , info ) => {
+        if(err) {
+            console.log('Error in sending mail',err) ;
+            return;
+
+        } 
+        console.log('Message sent' , info) ;
+        return ;
+        
+    });
+}
